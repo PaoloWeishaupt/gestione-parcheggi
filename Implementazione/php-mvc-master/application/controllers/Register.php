8a0d1a734a10b367690649655df1846d3e8b6dee
Paolo Weishaupt <paolo.weishaupt@gmail.com>
2019-10-18 20:22:59 +0200

    create Registration controller

---
+++ Implementazione/php-mvc-master/application/controllers/Register.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Controller per la pagina home.
+ */
+namespace controllers;
+
+use Libs\Auth as Auth;
+use Libs\ViewLoader as ViewLoader;
+use Models\RegisterModel as RegisterModel;
+
+class Register
+{
+    public function index()
+    {
+        ViewLoader::load('register/index');
+    }
+
+    public function register()
+    {
+        RegisterModel::register();
+    }
+}
